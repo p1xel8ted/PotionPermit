c1d906e129ceb6862a5dab43fcc4a26dfc9452dc
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2022-09-24 20:28:16 +1000

    Initial commit
    Initial commit

---
+++ PotionPermit/Patches.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+
+namespace PotionPermit;
+
+[HarmonyPatch]
+public class Patches
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(SettingsManager), nameof(SettingsManager.LOAD))]
+    public static void Load()
+    {
+        SettingsManager.resolutionList.Add(Plugin.Resolution);
+    }
+    
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(GraphicSettingUI), nameof(GraphicSettingUI.SetGraphicLayout))]
+    public static void SetGraphicLayout(ref GraphicSettingUI __instance)
+    {
+        __instance.resolutionAr.AddItem(Plugin.Resolution);
+    }
+}
