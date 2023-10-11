9ac2ff05794d8e0db83f833f3cec1428fd1b30db
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-10-11 14:49:18 +1100

    Add CharacterBobPatches.cs file with Harmony patches for disabling character bobbing and changing doggy animation when idle.

---
+++ AnAlchemicalCollection/Patches/CharacterBobPatches.cs
@@ -0,0 +1,53 @@
+﻿using CharacterChemist.Animation;
+using GlobalEnum;
+using HarmonyLib;
+
+namespace AnAlchemicalCollection;
+
+[Harmony]
+public static class CharacterBobPatches
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(CharacterAnimationBase), nameof(CharacterAnimationBase.Play))]
+    public static void CharacterAnimationBase_Play(ref CharacterAnimationBase __instance, ref float durationAnimation,
+        ref int totalFrame)
+    {
+        if (!Plugin.DisableCharacterBob.Value) return;
+        if (__instance.selectedAnimation != TypeAnimation.Idle) return;
+        durationAnimation = 0;
+        totalFrame = 0;
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(CharacterAnimationNPC), nameof(CharacterAnimationNPC.Play))]
+    public static void CharacterAnimationNPC_Play(ref CharacterAnimationNPC __instance, ref float durationAnimation,
+        ref int totalFrame)
+    {
+        if (!Plugin.DisableCharacterBob.Value) return;
+        if (__instance.selectedAnimation != TypeAnimation.Idle) return;
+        durationAnimation = 0;
+        totalFrame = 0;
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(CharacterAnimationPlayer), nameof(CharacterAnimationPlayer.Play))]
+    public static void CharacterAnimationPlayer_Play(ref CharacterAnimationPlayer __instance,
+        ref float durationAnimation, ref int totalFrame)
+    {
+        if (!Plugin.DisableCharacterBob.Value) return;
+        if (__instance.selectedAnimation != TypeAnimation.Idle) return;
+        durationAnimation = 0;
+        totalFrame = 0;
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(CharacterAnimationDoggy), nameof(CharacterAnimationDoggy.Play))]
+    public static void CharacterAnimationDoggy_Play(ref CharacterAnimationDoggy __instance)
+    {
+        if (!Plugin.DisableCharacterBob.Value) return;
+        if (__instance.currentAnimation == TypeAnimation.Dogie_Idle)
+        {
+            __instance.currentAnimation = TypeAnimation.Dogie_SitIdle;
+        }
+    }
+}
